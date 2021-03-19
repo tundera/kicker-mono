4a8979d433724b70ed5cfeadbb63f9c63ddd23f9
tundera <tunderadev@gmail.com>
2021-03-18 18:07:01 -0600

    make examples type safe

---
+++ scripts/log-to-console.ts
@@ -0,0 +1,17 @@
+#!/usr/bin/env ts-node-script
+
+const logToConsole = async () => {
+  console.log("Logged to console");
+};
+
+const main = async () => {
+  try {
+    await logToConsole();
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
+main().finally(() => {
+  console.log("Done! 🥳");
+});
