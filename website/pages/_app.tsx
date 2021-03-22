fda7291803334f1f001e52f7cbf3ec4a9ecc3ce0
tundera <tunderadev@gmail.com>
2021-03-22 12:52:56 -0600

    update cli opts

---
+++ website/pages/_app.tsx
@@ -0,0 +1,36 @@
+import type {AppProps } from 'next/app'
+
+import { DokzProvider, GithubLink, ColorModeSwitch } from 'dokz'
+import Head from 'next/head'
+import { ChakraProvider } from '@chakra-ui/react'
+
+export default function App({ Component, pageProps }: AppProps) {
+    return (
+        <ChakraProvider resetCSS>
+            <Head>
+                <link
+                    href='https://fonts.googleapis.com/css?family=Fira+Code'
+                    rel='stylesheet'
+                    key='google-font-Fira'
+                />
+            </Head>
+            <DokzProvider
+                headerItems={[
+                    <GithubLink
+                        key='0'
+                        url='https://github.com/remorses/dokz'
+                    />,
+                    <ColorModeSwitch key='1' />,
+                ]}
+                sidebarOrdering={{
+                    'index.mdx': true,
+                    Documents_Group: {
+                        'another.mdx': true,
+                    },
+                }}
+            >
+                <Component {...pageProps} />
+            </DokzProvider>
+        </ChakraProvider>
+    )
+}
