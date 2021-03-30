3f9625c6c9875dc2b6e456fe25b5dae3318bb173
tundera <tunderadev@gmail.com>
2021-03-29 21:23:56 -0600

    correct core test utils to use dehydrated state;

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { CustomAppProps } from 'types'
 
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 import { QueryClient, useQueryErrorResetBoundary } from 'react-query'
 import { ErrorBoundary } from 'react-error-boundary'
 import { Provider as AuthProvider } from 'next-auth/client'
@@ -11,11 +11,14 @@ import QueryProvider from 'src/components/providers/QueryProvider'
 import ThemeProvider from 'src/components/providers/ThemeProvider'
 import RootErrorFallback from 'src/components/utility/RootErrorFallback'
 
-const queryClient = new QueryClient()
-
 function MyApp({ Component, pageProps, router }: CustomAppProps) {
   const { reset } = useQueryErrorResetBoundary()
   const getLayout = Component.getLayout || ((page) => page)
+
+  const queryClientRef = useRef<QueryClient>()
+  if (!queryClientRef.current) {
+    queryClientRef.current = new QueryClient()
+  }
 
   useEffect((): void => {
     splitbee.init({
@@ -31,7 +34,7 @@ function MyApp({ Component, pageProps, router }: CustomAppProps) {
       onReset={reset}
     >
       <ThemeProvider>
-        <QueryProvider client={queryClient} state={pageProps.dehydratedState}>
+        <QueryProvider client={queryClientRef.current} state={pageProps.dehydratedState}>
           <AuthProvider session={pageProps.session}>
             <FormProvider>{getLayout(<Component {...pageProps} />)}</FormProvider>
           </AuthProvider>
